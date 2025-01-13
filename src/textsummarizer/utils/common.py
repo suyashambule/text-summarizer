a775426470a76a557f82498ca65a0efe3f964cb9
suyash <suyashambule1234@gmail.com>
2025-01-14 03:20:17 +0530

    some changes

--- src/textsummarizer/utils/common.py
+++ src/textsummarizer/utils/common.py
@@ -1,21 +1,29 @@
 import os
 from box.exceptions import BoxValueError
+import yaml
 from src.textsummarizer.logging import logging
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
-import yaml
+
 
 @ensure_annotations
-def read_yaml(path_to_yaml:Path)->ConfigBox:
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
-        with open (path_to_yaml) as yaml_file:
-            contents=yaml.safe_load(yaml_file)
-            logging.INFO(f"yaml {path_to_yaml} is open successfully")
-            return ConfigBox(contents)
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
     except BoxValueError:
-        raise ValueError("yaml file is Empty")
+        raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logging.info(f"created directory at: {path}")
 
